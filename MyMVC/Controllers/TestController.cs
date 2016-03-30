4566b5d2e0fdafb87581a3cfa678158cc04ace5b
nigel-edwards <nigel.edwards@cwa.ac.uk>
2016-03-30 20:58:41 +0100

    Initial Hello World Project

---
+++ MyMVC/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public string GetString()
+        {
+            return "Some string ";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+	}
+}
